76706508e885950bc8b8e835c3de9d610b0a8eb8
Timi-of-Earth <shodiyatimilehin@gmail.com>
2021-06-22 14:48:10 +0100

    Updates

---
+++ precache-manifest.e2926532867af845c02b13502c6f3a0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce7f789b6dbac0bef2d79d4c33a5b5f",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "0f1839fae63d3092ce9c",
+    "url": "/rock-paper-scissors/static/css/main.94b236eb.chunk.css"
+  },
+  {
+    "revision": "4ce18248550395b712cd",
+    "url": "/rock-paper-scissors/static/js/2.e1a76238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.e1a76238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1839fae63d3092ce9c",
+    "url": "/rock-paper-scissors/static/js/main.b37bb27c.chunk.js"
+  },
+  {
+    "revision": "54d8b3ee6fc233b06722",
+    "url": "/rock-paper-scissors/static/js/runtime-main.5af9a2bb.js"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/rock-paper-scissors/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "f19a40fbc5296effc230454fa1295890",
+    "url": "/rock-paper-scissors/static/media/image-rules-bonus.f19a40fb.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/rock-paper-scissors/static/media/image-rules.5d74d25d.svg"
+  }
+]);
